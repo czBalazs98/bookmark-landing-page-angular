171633e12c7754f66ac7b72ac12c3eff9c3b5ddc
bczmarko <bczmarko@intrend.hu>
2023-11-06 12:25:22 +0100

    welcome component implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/components/header/header.component';
 import { MobileNavComponent } from './common/components/mobile-nav/mobile-nav.component';
+import { WelcomeComponent } from './bookmark/components/welcome/welcome.component';
+import { BookmarkViewComponent } from './bookmark/components/bookmark-view/bookmark-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MobileNavComponent
+    MobileNavComponent,
+    WelcomeComponent,
+    BookmarkViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/bookmark/components/bookmark-view/bookmark-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmark-view',
+  templateUrl: './bookmark-view.component.html',
+  styleUrls: ['./bookmark-view.component.scss']
+})
+export class BookmarkViewComponent {
+
+}
